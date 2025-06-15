f9d63b78f94a90e5cd467b0da775e463cba86735
mollarath <mollarath.likit@gmail.com>
2025-06-16 01:55:32 +0700

    make the digits hours, minutes and seconds to be 2

--- digitalWatch/app.js
+++ digitalWatch/app.js
@@ -7,6 +7,10 @@ function getTime() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
+    hours = String(hours).padStart(2,'0');
+    minutes = String(minutes).padStart(2,'0');
+    seconds = String(seconds).padStart(2,'0');
+
     displayTime.textContent = `${hours}:${minutes}:${seconds}`;
     // console.log(hours, minutes, seconds);
 }
