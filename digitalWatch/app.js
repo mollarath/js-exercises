e50e3a26dd7a1dc5e80964b2305b8d0f5038316c
mollarath <mollarath.likit@gmail.com>
2025-06-17 13:08:59 +0700

    center div, add date and formatTime

--- digitalWatch/app.js
+++ digitalWatch/app.js
@@ -1,18 +1,27 @@
 const displayTime = document.getElementById("displayTime");
+const displayDate = document.getElementById("displayDate");
+const timeFormat = document.getElementById("timeFormat");
 
 function getTime() {
+    const weekName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     const date = new Date();
+
+    let weekDayNum = date.getDay();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
 
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-    hours = String(hours).padStart(2,'0');
-    minutes = String(minutes).padStart(2,'0');
-    seconds = String(seconds).padStart(2,'0');
+    timeFormat.textContent = hours >= 12 ? `PM` : `AM`;
 
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    displayDate.textContent = `${weekName[weekDayNum]} ${day}/${month}/${year}`;
     displayTime.textContent = `${hours}:${minutes}:${seconds}`;
-    // console.log(hours, minutes, seconds);
 }
 
 setInterval(getTime, 1000);
