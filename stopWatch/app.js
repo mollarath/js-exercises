0d696a3e2d9428069dac61a581a2129e70610631
mollarath <mollarath.likit@gmail.com>
2025-06-18 14:53:52 +0700

    add lap feature and more function to buttons

--- stopWatch/app.js
+++ stopWatch/app.js
@@ -3,18 +3,32 @@ startBtn = document.getElementById("startBtn");
 stopBtn = document.getElementById("stopBtn");
 resetBtn = document.getElementById("resetBtn");
 lapBtn = document.getElementById("lapBtn");
+lapList = document.getElementById("lapList");
 
 let startTime;
 let elaspedTime = 0;
 let interval;
+let hours;
+let minutes;
+let seconds;
+let milliSeconds;
 isRunning = false;
+
+const make2Digit = (num) => {
+    return String(num).padStart(2, "0");
+};
 
 const start = () => {
     if (!isRunning) {
+        startBtn.style.display = "none";
+        stopBtn.style.display = "inline";
         startTime = Date.now() - elaspedTime;
         isRunning = true;
         interval = setInterval(update, 10);
     }
+
+    stopBtn.style.display = "inline";
+    resetBtn.style.display = "inline";
     lapBtn.style.display = "inline";
 };
 
@@ -22,6 +36,9 @@ const stop = () => {
     if (isRunning) {
         isRunning = false;
         clearInterval(interval);
+        stopBtn.style.display = "none";
+        startBtn.textContent = "Continue";
+        startBtn.style.display = "inline";
     }
 };
 
@@ -30,25 +47,40 @@ const reset = () => {
     clearInterval(interval);
     elaspedTime = 0;
     displayTime.textContent = `00:00:00:00`;
+    stopBtn.style.display = "none";
+    resetBtn.style.display = "none";
     lapBtn.style.display = "none";
+    while (lapList.firstChild) {
+        console.log(lapList.firstChild);
+        lapList.removeChild(lapList.firstChild);
+    }
+    startBtn.textContent = "Start";
+    startBtn.style.display = "inline";
 };
 
 const update = () => {
     let currentTime = Date.now();
     elaspedTime = currentTime - startTime;
 
-    let hours = Math.floor((elaspedTime / (60 * 60 * 100)) % 60);
-    let minutes = Math.floor((elaspedTime / (60 * 1000)) % 60);
-    let seconds = Math.floor((elaspedTime / 1000) % 60);
-    let milliSeconds = Math.floor((elaspedTime % 1000) / 10);
+    hours = Math.floor((elaspedTime / (60 * 60 * 100)) % 60);
+    minutes = Math.floor((elaspedTime / (60 * 1000)) % 60);
+    seconds = Math.floor((elaspedTime / 1000) % 60);
+    milliSeconds = Math.floor((elaspedTime % 1000) / 10);
 
-    hours = String(hours).padStart(2, "0");
-    minutes = String(minutes).padStart(2, "0");
-    seconds = String(seconds).padStart(2, "0");
-    milliSeconds = String(milliSeconds).padStart(2, "0");
+    hours = make2Digit(hours);
+    minutes = make2Digit(minutes);
+    seconds = make2Digit(seconds);
+    milliSeconds = make2Digit(milliSeconds);
     displayTime.textContent = `${hours}:${minutes}:${seconds}:${milliSeconds}`;
+};
+
+const addLap = () => {
+    let li = document.createElement("li");
+    li.textContent = `${hours}:${minutes}:${seconds}:${milliSeconds}`;
+    lapList.append(li);
 };
 
 startBtn.addEventListener("click", start);
 stopBtn.addEventListener("click", stop);
 resetBtn.addEventListener("click", reset);
+lapBtn.addEventListener("click", addLap);
