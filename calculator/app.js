7f8d34e59619f3f43d800831d62b94731881860c
mollarath <mollarath.likit@gmail.com>
2025-07-01 16:37:37 +0700

    finish decoration for calculator

--- calculator/app.js
+++ calculator/app.js
@@ -1,1 +1,10 @@
+const displayNum = document.getElementById("displayNum");
+const btn = document.querySelectorAll("button");
 
+btn.forEach((element) => {
+    console.log(element.textContent);
+});
+
+btn.addEventListener((ele) => {
+    console.log(ele);
+});
